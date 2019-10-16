690a167295c0ff0057a7ddc80277b5d0e8ce2512
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-16 12:09:41 +0800

    Add getAll todo test

--- src/test/java/com/tw/api/unit/test/controller/TodoControllerTest.java
+++ src/test/java/com/tw/api/unit/test/controller/TodoControllerTest.java
@@ -12,8 +12,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -45,6 +48,33 @@ class TodoControllerTest {
                 .andExpect(jsonPath("$.id", is(0)))
                 .andExpect(jsonPath("$.title", is("todo title")))
                 .andExpect(jsonPath("$.completed", is(false)))
+        ;
+    }
+
+    @Test
+    void getAll() throws Exception {
+        //given
+        List<Todo> todoList = new ArrayList<>();
+        todoList.add(new Todo(1, "don't get zucced", false, 1));
+        todoList.add(new Todo(2, "get out of facebook jail", false, 2));
+
+        when(todoRepository.getAll()).thenReturn(todoList);
+        //when
+        ResultActions result = mvc.perform(get("/todos"));
+        //then
+        result.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(2)))
+
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].title", is("don't get zucced")))
+                .andExpect(jsonPath("$[0].completed", is(false)))
+                .andExpect(jsonPath("$[0].order", is(1)))
+
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].title", is("get out of facebook jail")))
+                .andExpect(jsonPath("$[1].completed", is(false)))
+                .andExpect(jsonPath("$[1].order", is(2)))
         ;
     }
 }
